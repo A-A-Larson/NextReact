c91d7f3ac2bc20b2edbd0ba463121dbc9c0eb4e2
Andrew Larson <aalarson@mail.mccneb.edu>
2024-01-02 09:03:53 -0600

    Darkmode v2

--- nextportfolio/src/app/page.jsx
+++ nextportfolio/src/app/page.jsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
 import ThemeToggle from './components/ThemeToggle'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main 
+      className="flex min-h-screen flex-col items-center justify-between p-24
+      dark:bg-white"
+    >
       <ThemeToggle />
     </main>
   )
