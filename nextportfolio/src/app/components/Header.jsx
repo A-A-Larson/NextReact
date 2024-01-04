ca91f103c64fe54a66867067e1c08a0d9ee74206
drewdarko0 <65877684+drewdarko0@users.noreply.github.com>
2024-01-03 20:23:38 -0600

    Floating Hands v1

--- nextportfolio/src/app/components/Header.jsx
+++ nextportfolio/src/app/components/Header.jsx
@@ -1,17 +1,29 @@
 "use client";
 import React from 'react';
-import HeroImg from '../img/HeaderEdit2.png';
+import HeroImg from '../img/HeaderEdit4Resize.png';
+import Hands from '../img/HandsResize.png';
 import Image from 'next/image';
 
 export default function Header() {
     return (
         <div>
-            <Image                
-                width={3024}
+            
+            <Image
+                className='fixed -z-10'                
+                width={2930}
                 height={924}
                 src={HeroImg}
                 alt='Man with hands overlapping the top of the webpage as if he is holding onto it'
             />
+            <Image       
+                className='z-0'         
+                width={2930}
+                height={924}
+                src={Hands}
+                alt='Floating hands'
+            />
+            
+            
             <h1
                 className=''
             >Andrew Larson</h1>
