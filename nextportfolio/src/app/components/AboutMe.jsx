c52a07a0b4f2c7cba11a8b9af259ecf8e9b42292
drewdarko0 <65877684+drewdarko0@users.noreply.github.com>
2024-12-08 11:03:36 -0600

    Feet resize, project/aboutme animations removal

--- nextportfolio/src/app/components/AboutMe.jsx
+++ nextportfolio/src/app/components/AboutMe.jsx
@@ -1,34 +1,31 @@
 import Image from 'next/image';
 import About from '../img/aboutbust1.png';
-import { useInView } from "react-intersection-observer";
 
 export default function AboutMe() {
     
-    const { ref: aboutRef, inView: aboutIsVisible } = useInView({"triggerOnce": true});
     return (
         <div
-            ref={aboutRef}
             className='aboutLayout 
                       text-lightHdrClr 
                       dark:text-darkHdrClr'>
 
-            <h1 className={`${'aboutH1'} ${aboutIsVisible && 'aboutH1-animation'}`}>About Me</h1>
+            <h1 className='aboutH1'>About Me</h1>
 
-            <div className={`${'about1'} ${aboutIsVisible ? 'about1-animation' : ''}`}>
+            <div className='about1'>
               <h2 className="aboutH2">What am I doing now?</h2>
               <p className='aboutP1'>
               I am finishing a degree in full stack web development with a certificate in computer programming and another in web and mobile app development while doing an internship at a data analytics firm.
               </p>
             </div>
 
-            <div className={`${'about2'} ${aboutIsVisible ? 'about2-animation' : ''}`}>
+            <div className='about2'>
               <h2 className="aboutH2">How did I get here?</h2>
               <p className='aboutP2'>
               In 2007 I started a career as a mechanic with a degree in automotive technology. Since then I&apos;ve been a self starter, working independently in a commision shop with tight deadlines. A mechanical team lead on a production line that used Six Sigma methodology and Lean production with weekly sprints. I&apos;ve achieved the title of Master Automotive Techician and run out of new things to learn. <br /><br />As a lifelong learner, I&apos;m starting a new chapter in my life as I trade in my wrenches for a keyboard.
               </p>
             </div>
 
-            <div className={`${'about3'} ${aboutIsVisible ? 'about3-animation' : ''}`}>
+            <div className='about3'>
               <h2 className="aboutH2">Where do I want to go from here?</h2>
               <p className='aboutP3'>
               As I approach graduation I will continue working on projects, building my portfolio and looking for opportunities to gain real world experience. Use the email button in the navigation bar or footer to connect!
--- nextportfolio/src/app/components/Project.jsx
+++ nextportfolio/src/app/components/Project.jsx
@@ -1,27 +1,23 @@
-import { useInView } from "react-intersection-observer";
-
 export default function Project() {
-    
-  const { ref: projectRef, inView: projectIsVisible } = useInView({"triggerOnce": true});
+   
 
     return (
         <div
-              ref={projectRef}
               className='projectDiv 
                         text-lightHdrClr 
                         dark:text-darkHdrClr'>
-            <h1 className={`${'projectH1'} ${projectIsVisible ? 'projectH-animation' : ''}`}>This Project</h1> <br/>
-            <h2 className={`${'projectH2-1'} ${projectIsVisible ? 'projectH-animation' : ''}`}>Goals</h2>
+            <h1 className='projectH1'>This Project</h1> <br/>
+            <h2 className='projectH2-1'>Goals</h2>
             
             <ul className="goalsul">
-              <li className={`${'goalsli1'} ${projectIsVisible ? 'goalsli1-animation' : ''}`}>Build a modular portfolio I can add to over time</li>
-              <li className={`${'goalsli2'} ${projectIsVisible ? 'goalsli2-animation' : ''}`}>Get more experience using Next.js, Tailwind CSS, and React</li>
-              <li className={`${'goalsli3'} ${projectIsVisible ? 'goalsli3-animation' : ''}`}>Practiced what I&apos;ve learned of responsive web design and accessibility</li>
+              <li className='goalsli1'>Build a modular portfolio I can add to over time</li>
+              <li className='goalsli2'>Get more experience using Next.js, Tailwind CSS, and React</li>
+              <li className='goalsli3'>Practiced what I&apos;ve learned of responsive web design and accessibility</li>
             </ul>
-            <h2 className={`${'projectH2-2'} ${projectIsVisible ? 'projectH-animation' : ''}`}>Opportunities</h2>
+            <h2 className='projectH2-2'>Opportunities</h2>
             <ul className="oppul">
-              <li className={`${'oppli1'} ${projectIsVisible ? 'oppli1-animation' : ''}`}>Learned about animations, content creation and even photography</li>
-              <li className={`${'oppli2'} ${projectIsVisible ? 'oppli2-animation' : ''}`}>Learned about polyfills and scripts to get animations to work on more browsers</li>
+              <li className='oppli1'>Learned about animations, content creation and even photography</li>
+              <li className='oppli2'>Learned about polyfills and scripts to get animations to work on more browsers</li>
             </ul>
         </div>
     )
